334ae014986542bc29bfbf2ec6e55415a2acfc0c
Yungi <zoshs27@gmail.com>
2021-08-25 15:26:13 +0900

    Change a variable's name

--- ExtractRatio.py
+++ ExtractRatio.py
@@ -1,4 +1,4 @@
-def ExtractRatio(full_df:pd.DataFrame) -> pd.DataFrame():
+def ExtractRatio(date_dataset:pd.DataFrame) -> pd.DataFrame():
     '''
     input DataFrame must have at least full-time (a resolution of 5 min) values for one day.
     This Function extracts the ratio in terms of its 95% percentile value for that day.
@@ -19,21 +19,21 @@ def ExtractRatio(full_df:pd.DataFrame) -> pd.DataFrame():
     SpatialRatioDist(slink_loc, target_data, truncate=True, minval=0.2, maxval=1)
     '''
     
-    LMV = full_df.loc[:, ['PRCS_YEAR', 'PRCS_MON', 'PRCS_DAY', 'LINK_ID', 'PRCS_SPD']].groupby(['PRCS_YEAR', 'PRCS_MON', 'PRCS_DAY', 'LINK_ID']).quantile(0.95).rename({'PRCS_SPD':'LMV'}, axis='columns')
+    LMV = date_dataset.loc[:, ['PRCS_YEAR', 'PRCS_MON', 'PRCS_DAY', 'LINK_ID', 'PRCS_SPD']].groupby(['PRCS_YEAR', 'PRCS_MON', 'PRCS_DAY', 'LINK_ID']).quantile(0.95).rename({'PRCS_SPD':'LMV'}, axis='columns')
     LMV = LMV.reset_index().sort_values(['PRCS_YEAR', 'PRCS_MON', 'PRCS_DAY', 'LINK_ID'], ascending=True)
-    full_df = full_df.sort_values(['PRCS_YEAR', 'PRCS_MON', 'PRCS_DAY', 'LINK_ID'], ascending=True)
+    date_dataset = date_dataset.sort_values(['PRCS_YEAR', 'PRCS_MON', 'PRCS_DAY', 'LINK_ID'], ascending=True)
     
-    one_day = int(full_df.shape[0] / LMV.shape[0])
+    one_day = int(date_dataset.shape[0] / LMV.shape[0])
     if one_day != 288: # 5 min resolution; One day = 24hours * 60 mins / 5 min resolution = 288 
         raise Warning("DataFrame seems to have some missing time-series data")
 
     start = 0
     ratio_list = []
     for lmv in LMV['LMV'].values:
-        rslt = full_df[start:start+one_day]['PRCS_SPD'] / lmv
+        rslt = date_dataset[start:start+one_day]['PRCS_SPD'] / lmv
         rslt = rslt.to_list()
         ratio_list.extend(rslt)
         start += one_day
         
-    full_df['ratio'] = ratio_list
-    return full_df
+    date_dataset['ratio'] = ratio_list
+    return date_dataset
